96d5d3ee828e9c0f81902e440a9c836b8828c06e
Sathvik Reddy <sathvikreddy1985@gmail.com>
2022-06-08 15:02:56 +0530

    changed react-app-2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect,useState} from "react";
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDateTimePicker } from '@mui/x-date-pickers/DesktopDateTimePicker';
+import Stack from '@mui/material/Stack';
+import axios from "axios"
+
 
 function App() {
+
+  const [fetchdata,setFetchdata] = useState(false);
+  const [data, setData] = useState([]);
+  const [value, setValue] = React.useState(new Date(Date.now()));
+  const [files, setFiles] = useState()
+
+
+
+const onSubmitHandler =(e) =>{
+e.preventDefault();
+const timestamp = Date.parse(value)/1000;
+
+
+  const formData = new FormData();
+
+  for (const file of e.target.files.files){
+
+    formData.append("files",file)
+  }
+  
+
+  
+axios.post(`http://api:3000/uploadfile?date=${timestamp}`,formData)
+
+
+}
+
+
+ 
+
+  useEffect(() =>{
+    fetch("http://api:3000/image-gallery").then(data => data.json()).then(pathData => setData(pathData) )
+  },[fetchdata])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Stack spacing={3}>
+        <DesktopDateTimePicker
+          label="For desktop"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </Stack>
+    </LocalizationProvider>
+    <input type="file" name="files"  multiple/>
+    <button type="submit" >Submit Image&time</button>
+    </form>
     </div>
+    <div className="App">
+      <hr />
+      <center><bold><h1>Image Gallery</h1></bold></center>
+      <button onClick={() => setFetchdata(!fetchdata) }>fetch</button>
+      <div className="box">
+      {data.map(image => {
+       return (
+        
+       
+           
+       <img src={`http://api:3000${image.path}`}  width="300px" height="300px" />
+       
+
+     
+  
+        )
+      })}
+</div>
+    </div>
+    </>
   );
 }
 
